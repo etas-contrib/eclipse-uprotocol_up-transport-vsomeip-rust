e4b8d36229b4d0f00af2d0455ca65870b60a0f09
Kai Hudalla <kai.hudalla@bosch.com>
2026-08-04 09:49:56 +0200

    Remove reading values from env context

--- .github/scripts/build-envsetup.sh
+++ .github/scripts/build-envsetup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-# Sets up the environment variables for c++ stdlib paths.
+# Sets up the environment variables for c++ stdlib paths and
+# adds them to the GH Actions Step output.
 
 # shellcheck source=../../build/envsetup.sh
 source "$GITHUB_WORKSPACE/build/envsetup.sh" highest
@@ -8,5 +9,5 @@ source "$GITHUB_WORKSPACE/build/envsetup.sh" highest
 echo "ARCH_SPECIFIC_CPP_STDLIB_PATH=$ARCH_SPECIFIC_CPP_STDLIB_PATH"
 echo "GENERIC_CPP_STDLIB_PATH=$GENERIC_CPP_STDLIB_PATH"
 echo "VSOMEIP_INSTALL_PATH=$RUNNER_TEMP/vsomeip-install"
-} >> "$GITHUB_ENV"
+} >> "$GITHUB_OUTPUT"
 mkdir -p "$RUNNER_TEMP/vsomeip-install"
